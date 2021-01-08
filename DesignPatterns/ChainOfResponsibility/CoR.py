756dac9e8665c45b1ffc90ebff9c217c9323d2b2
HongfeiXu <icevmj@gmail.com>
2021-01-08 16:29:01 +0800

    生成器模式、原型模式

---
+++ DesignPatterns/ChainOfResponsibility/CoR.py
@@ -0,0 +1,13 @@
+# !/usr/bin/env python
+# -*-coding:utf-8 -*-
+
+"""
+# File       : CoR.py.py
+# Time       ：2021/1/8 16:24
+# Author     ：author name
+# version    ：python 3.7
+# Description：
+"""
+
+# refs: https://refactoringguru.cn/design-patterns/chain-of-responsibility
+# 责任链模式
